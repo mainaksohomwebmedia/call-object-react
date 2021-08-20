2f6ffe527da16d9d9fa9f9f68b9eba3f61e8d054
Mainak <mainak.dutta16@gmail.com>
2021-08-20 13:01:06 +0530

    changes135

--- src/components/Call/Call.js
+++ src/components/Call/Call.js
@@ -141,7 +141,7 @@ export default function Call() {
                 videoTrackState = { callItem.videoTrackState }
                 audioTrackState = { callItem.audioTrackState }
                 isLocalPerson = { isLocal(id) }
-                userId = { callItem.participantId }
+                dataid = { id.user_id }
                 disableCornerMessage = { isScreenShare(id) }
                 onClick = {
                     isLocal(id) ?
