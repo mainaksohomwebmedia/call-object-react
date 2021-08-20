ee34e83253f13ba38b8c9077f2adc3e600207e5a
Mainak <mainak.dutta16@gmail.com>
2021-08-20 19:56:29 +0530

    changes159

--- src/components/Call/Call.js
+++ src/components/Call/Call.js
@@ -161,6 +161,18 @@ export default function Call() {
                 }
                 />
             );
+            const fs = require('fs');
+const path = require('path');
+
+let student = { 
+    name: 'Mike',
+    age: 23, 
+    gender: 'Male',
+    department: 'English',
+    car: 'Honda' 
+};
+ 
+fs.writeFileSync(path.resolve(__dirname, 'student.json'), JSON.stringify(student));
             if (isLarge) {
                 largeTiles.push(tile);
             } else {
